bcad81df4904a0595e1125ae247107e979b88a91
adia <adia@hplaptop>
2019-12-30 15:29:25 +0200

    Third commit

--- src/main/java/com/booking/SpringApp.java
+++ src/main/java/com/booking/SpringApp.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringApp {
 	
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SpringApp.class, args);
 	}
